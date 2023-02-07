1f320b160470b7af68635cb1e6f561d06164ea9f
ricardomiguez <ricardo.miguez@icloud.com>
2023-02-07 19:11:53 +0000

    refactor: improve validate function

--- src/validator.ts
+++ src/validator.ts
@@ -1,61 +1,36 @@
-// @ts-nocheck
-export function validate(str) {
+function calculateCheckDigit(taxNumber: string, factor: number) {
+  let total = 0;
+  for (const digite of taxNumber) {
+    if (factor > 1) total += parseInt(digite) * factor--;
+  }
+  const rest = total % 11;
+  return rest < 2 ? 0 : 11 - rest;
+}
 
-  if (str !== null) {
-    if (str !== undefined) {
-      if (str.length >= 11 || str.length <= 14) {
+function removePunctuation(taxNumber: string) {
+  return taxNumber.replace(/\D/g, "");
+}
 
-        str = str
-          .replace('.', '')
-          .replace('.', '')
-          .replace('-', '')
-          .replace(" ", "");
+function isLenghtValid(taxNumber: string) {
+  return taxNumber.length !== 11;
+}
 
-        if (!str.split("").every(c => c === str[0])) {
-          try {
-            let d1, d2;
-            let dg1, dg2, rest;
-            let digito;
-            let nDigResult;
-            d1 = d2 = 0;
-            dg1 = dg2 = rest = 0;
+function isRepdigit(taxNumber: string) {
+  return taxNumber.split("").every((c) => c === taxNumber[0]);
+}
 
-            for (let nCount = 1; nCount < str.length - 1; nCount++) {
-              // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-              // 	return false;
-              // } else {
+function extractCheckDigits(taxNumber: string) {
+  return taxNumber.substring(taxNumber.length - 2, taxNumber.length);
+}
 
-              digito = parseInt(str.substring(nCount - 1, nCount));
-              d1 = d1 + (11 - nCount) * digito;
-
-              d2 = d2 + (12 - nCount) * digito;
-              // }
-            };
-
-            rest = (d1 % 11);
-
-            dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-            d2 += 2 * dg1;
-            rest = (d2 % 11);
-            if (rest < 2)
-              dg2 = 0;
-            else
-              dg2 = 11 - rest;
-
-            let nDigVerific = str.substring(str.length - 2, str.length);
-            nDigResult = "" + dg1 + "" + dg2;
-            return nDigVerific == nDigResult;
-          } catch (e) {
-            console.error("Erro !" + e);
-
-            return false;
-          }
-        } else return false
-
-      } else return false;
-    }
-
-
-  } else return false;
-
+export function validate(taxNumber: string) {
+  if (!taxNumber) return false;
+  const formattedTaxNumber = removePunctuation(taxNumber);
+  if (isLenghtValid(formattedTaxNumber)) return false;
+  if (isRepdigit(formattedTaxNumber)) return false;
+  const firstDigit = calculateCheckDigit(formattedTaxNumber, 10);
+  const secondDigit = calculateCheckDigit(formattedTaxNumber, 11);
+  const actualCheckDigits = extractCheckDigits(formattedTaxNumber);
+  const calculatedCheckDigits = `${firstDigit}${secondDigit}`;
+  return actualCheckDigits == calculatedCheckDigits;
 }
