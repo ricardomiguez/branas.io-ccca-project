67685aed78325ab49624be03b0ce03b65df4e2e1
ricardomiguez <ricardo.miguez@icloud.com>
2023-02-12 23:58:29 +0000

    feat: add checkout resource POST method

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,40 @@
-import express from "express";
-
+import express, { Request, Response } from "express";
+import { validate } from "./validator";
+import pgp from "pg-promise";
 const app = express();
+app.use(express.json());
+
+app.post("/checkout", async function (req: Request, res: Response) {
+  const output: Output = { total: 0 };
+  const connection = pgp()(
+    "postgres://postgres:123456@localhost:5432/ricardomiguez"
+  );
+  if (req.body.items) {
+    for (const item of req.body.items) {
+      const [productData] = await connection.query(
+        "select * from ccca.product where id_product = $1",
+        item.idProduct
+      );
+      output.total += parseFloat(productData.price) * item.quantity;
+    }
+  }
+  if (req.body.coupon) {
+    const [couponData] = await connection.query(
+      "select * from ccca.coupon where code = $1",
+      req.body.coupon
+    );
+    const percentage = parseFloat(couponData.percentage);
+    output.total -= (output.total * percentage) / 100;
+  }
+  const isValid = validate(req.body.taxNumber);
+  if (!isValid) output.message = "Invalid tax number";
+  await connection.$pool.end();
+  res.json(output);
+});
+
+type Output = {
+  total: number;
+  message?: string;
+};
 
 app.listen(3000);
