7e05c5be1d342364dd35f95ed414368c40dbaeb7
ricardomiguez <ricardo.miguez@icloud.com>
2023-02-16 00:41:34 +0000

    ops: update SQL scripts

--- create.sql
+++ create.sql
@@ -6,16 +6,23 @@ create schema ccca;
 create table ccca.product (
   id_product integer,
   description text,
-  price numeric
+  price numeric,
+  width integer,
+  height integer,
+  lenght integer,
+  weight numeric
 );
 
-insert into ccca.product (id_product, description, price) values (1, 'A', 1000);
-insert into ccca.product (id_product, description, price) values (2, 'B', 5000);
-insert into ccca.product (id_product, description, price) values (3, 'C', 30);
+insert into ccca.product (id_product, description, price, width, height, lenght, weight) values (1, 'A', 1000, 100, 30, 10, 3);
+insert into ccca.product (id_product, description, price, width, height, lenght, weight) values (2, 'B', 5000, 50, 50, 50, 22);
+insert into ccca.product (id_product, description, price, width, height, lenght, weight) values (3, 'C', 30, 10, 10, 10, 0.9);
+insert into ccca.product (id_product, description, price, width, height, lenght, weight) values (4, 'D', 30, -10, 10, 10, 0.9);
 
 create table ccca.coupon (
   code text,
-  percentage numeric
+  percentage numeric,
+  expire_date timestamp
 );
 
-insert into ccca.coupon (code, percentage) values ('DISCOUNT20', 20);
+insert into ccca.coupon (code, percentage, expire_date) values ('DISCOUNT20', 20, '2023-10-01T10:00:00');
+insert into ccca.coupon (code, percentage, expire_date) values ('DISCOUNT10', 10, '2022-10-01T10:00:00');
