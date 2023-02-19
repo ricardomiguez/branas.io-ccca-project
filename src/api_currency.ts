3b3ecee7652a7eaecfdc2208038e11e7be5e38d4
ricardomiguez <ricardo.miguez@icloud.com>
2023-02-19 17:41:26 +0000

    feat: add currency API currencies resource GET method

--- src/api_currency.ts
+++ src/api_currency.ts
@@ -1,5 +1,11 @@
-import express from "express";
-
+import express, { Request, Response } from "express";
 const app = express();
+app.use(express.json());
+
+app.get("/currencies", async function (req: Request, res: Response) {
+  res.json({
+    usd: 3 + Math.random(),
+  });
+});
 
 app.listen(3001);
