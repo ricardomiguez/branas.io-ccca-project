a1ae78d0701659465cc8cb097d8b32fc81788ede
ricardomiguez <ricardo.miguez@icloud.com>
2023-02-25 22:14:51 +0000

    feat: add shipping calculator resource class

---
+++ src/ShippingCalculator.ts
@@ -0,0 +1,9 @@
+export default class ShippingCalculator {
+  static calculate(product: any) {
+    const volume =
+      ((((product.width / 100) * product.height) / 100) * product.lenght) / 100;
+    const density = parseFloat(product.weight) / volume;
+    const itemShipping = 1000 * volume * (density / 100);
+    return itemShipping;
+  }
+}
