8464b9a17aeac32ec3d29240efd5c0350091aeb2
ricardomiguez <ricardo.miguez@icloud.com>
2023-02-26 20:52:36 +0000

    ops: update SQL scripts

--- create.sql
+++ create.sql
@@ -1,3 +1,5 @@
+drop table ccca.order;
+drop table ccca.item;
 drop table ccca.coupon;
 drop table ccca.product;
 drop schema ccca;
@@ -28,3 +30,17 @@ create table ccca.coupon (
 
 insert into ccca.coupon (code, percentage, expire_date) values ('DISCOUNT20', 20, '2023-10-01T10:00:00');
 insert into ccca.coupon (code, percentage, expire_date) values ('DISCOUNT10', 10, '2022-10-01T10:00:00');
+
+create table ccca.order (
+  id_order text,
+  tax_number text,
+  total numeric,
+  shipping numeric
+);
+
+create table ccca.item (
+  id_order text,
+  id_product integer,
+  price numeric,
+  quantity integer
+);
