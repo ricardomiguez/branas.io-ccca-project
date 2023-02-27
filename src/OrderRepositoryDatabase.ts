bd8968b641456d81bd634986abda57f69d0299e2
ricardomiguez <ricardo.miguez@icloud.com>
2023-02-27 19:55:30 +0000

    feat: update order repository class

--- src/OrderRepositoryDatabase.ts
+++ src/OrderRepositoryDatabase.ts
@@ -7,8 +7,8 @@ export default class orderRepositoryDatabase implements OrderRepository {
       "postgres://postgres:123456@localhost:5432/ricardomiguez"
     );
     await connection.query(
-      "insert into ccca.order (id_order, tax_number, total, shipping) values ($1, $2, $3, $4)",
-      [order.idOrder, order.taxNumber, order.total, order.shipping]
+      "insert into ccca.order (id_order, tax_number, code, total, shipping) values ($1, $2, $3, $4, $5)",
+      [order.idOrder, order.taxNumber, order.code, order.total, order.shipping]
     );
     for (const item of order.items) {
       await connection.query(
@@ -29,5 +29,17 @@ export default class orderRepositoryDatabase implements OrderRepository {
     );
     await connection.$pool.end();
     return orderData;
+  }
+
+  async count(): Promise<number> {
+    const connection = pgp()(
+      "postgres://postgres:123456@localhost:5432/ricardomiguez"
+    );
+    const [options] = await connection.query(
+      "select count(*) from ccca.order",
+      []
+    );
+    await connection.$pool.end();
+    return parseInt(options.count);
   }
 }
