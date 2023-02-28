6898fa234326b7ac93b160d2c8a304113d8af230
ricardomiguez <ricardo.miguez@icloud.com>
2023-02-28 20:02:24 +0000

    feat: add simulate shipping resource class

---
+++ src/SimulateShipping.ts
@@ -0,0 +1,35 @@
+import ProductRepository from "./ProductRepository";
+import ProductRepositoryDatabase from "./ProductRepositoryDatabase";
+
+export default class SimulateShipping {
+  constructor(
+    readonly productRepository: ProductRepository = new ProductRepositoryDatabase()
+  ) {}
+
+  async execute(input: Input): Promise<Output> {
+    const output: Output = { shipping: 0 };
+    if (input.items) {
+      for (const item of input.items) {
+        const productData = await this.productRepository.getProduct(
+          item.idProduct
+        );
+        const volume =
+          ((((productData.width / 100) * productData.height) / 100) *
+            productData.lenght) /
+          100;
+        const density = parseFloat(productData.weight) / volume;
+        const itemShipping = 1000 * volume * (density / 100);
+        output.shipping += Math.max(itemShipping, 10) * item.quantity;
+      }
+    }
+    return output;
+  }
+}
+
+type Input = {
+  items: { idProduct: number; quantity: number }[];
+};
+
+type Output = {
+  shipping: number;
+};
