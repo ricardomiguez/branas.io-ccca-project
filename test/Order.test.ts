48c988c316b8f58f35ba60979619c787fd92514c
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-02 23:03:15 +0000

    test: update order resource class tests

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,4 +1,9 @@
+import crypto from "crypto";
+import Order from "../src/Order";
+
 test("Should create an empty order", async function () {
+  const uuid = crypto.randomUUID();
+  const taxNumber = "407.302.170-27";
   const order = new Order(uuid, taxNumber);
   expect(order.getTotal()).toBe(0);
 });
