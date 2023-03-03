c7aab1986312e1e28cde168b05931be556eab952
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-03 19:35:23 +0000

    feat: update order resource class

--- src/Order.ts
+++ src/Order.ts
@@ -1,3 +1,4 @@
+import CurrencyTable from "./CurrencyTable";
 import Item from "./Item";
 import Product from "./Product";
 import TaxNumber from "./TaxNumber";
@@ -6,7 +7,11 @@ export default class Order {
   items: Item[];
   taxNumber: TaxNumber;
 
-  constructor(readonly idOrder: string, taxNumber: string) {
+  constructor(
+    readonly idOrder: string,
+    taxNumber: string,
+    readonly currencyTable: CurrencyTable = new CurrencyTable()
+  ) {
     this.items = [];
     this.taxNumber = new TaxNumber(taxNumber);
   }
@@ -15,13 +20,18 @@ export default class Order {
     if (quantity <= 0) throw new Error("Invalid quantity");
     if (this.items.some((item: Item) => item.idProduct === product.idProduct))
       throw new Error("Duplicated item");
-    this.items.push(new Item(product.idProduct, product.price, quantity));
+    this.items.push(
+      new Item(product.idProduct, product.price, quantity, product.currency)
+    );
   }
 
   getTotal() {
     let total = 0;
     for (const item of this.items) {
-      total += item.price * item.quantity;
+      total +=
+        item.price *
+        item.quantity *
+        this.currencyTable.getCurrency(item.currency);
     }
     return total;
   }
