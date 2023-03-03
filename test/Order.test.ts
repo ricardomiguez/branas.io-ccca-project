25a9b0b1d47ba0a0b2237ab30b38773c859ab8ca
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-03 19:33:36 +0000

    test: update order resource class tests

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -45,3 +45,15 @@ test("Should not add an item with quantity equal to or less than 0", async funct
     order.addItem(new Product(1, "A", 1000, 100, 30, 10, 3, "BRL"), -1)
   ).toThrow(new Error("Invalid quantity"));
 });
+
+test("Should create an order with 3 products, one of them in dollar currency", async function () {
+  const uuid = crypto.randomUUID();
+  const taxNumber = "407.302.170-27";
+  const currencyTable = new CurrencyTable();
+  currencyTable.addCurrency("USD", 3);
+  const order = new Order(uuid, taxNumber, currencyTable);
+  order.addItem(new Product(1, "A", 1000, 100, 30, 10, 3, "BRL"), 1);
+  order.addItem(new Product(2, "B", 5000, 50, 50, 50, 22, "USD"), 1);
+  order.addItem(new Product(3, "C", 30, 10, 10, 10, 0.9, "BRL"), 3);
+  expect(order.getTotal()).toBe(16090);
+});
