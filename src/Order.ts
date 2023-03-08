40df204b4a49b4ff251fe7e47d1a98a6ca9b0667
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-08 23:27:06 +0000

    feat: update order resource class

--- src/Order.ts
+++ src/Order.ts
@@ -4,16 +4,20 @@ import Product from "./Product";
 import TaxNumber from "./TaxNumber";
 
 export default class Order {
-  items: Item[];
-  taxNumber: TaxNumber;
+  readonly items: Item[];
+  readonly taxNumber: TaxNumber;
+  readonly code: string;
 
   constructor(
     readonly idOrder: string,
     taxNumber: string,
-    readonly currencyTable: CurrencyTable = new CurrencyTable()
+    readonly currencyTable: CurrencyTable = new CurrencyTable(),
+    readonly sequence: number = 1,
+    readonly date: Date = new Date()
   ) {
     this.items = [];
     this.taxNumber = new TaxNumber(taxNumber);
+    this.code = `${date.getFullYear()}${new String(sequence).padStart(8, "0")}`;
   }
 
   addItem(product: Product, quantity: number) {
@@ -23,6 +27,10 @@ export default class Order {
     this.items.push(
       new Item(product.idProduct, product.price, quantity, product.currency)
     );
+  }
+
+  getCode() {
+    return this.code;
   }
 
   getTotal() {
