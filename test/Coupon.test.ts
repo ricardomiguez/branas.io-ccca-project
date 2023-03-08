50a97b9f6bb0e5e4b72f45c73b299fd2d35a4522
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-08 23:13:49 +0000

    test: update coupon resource class tests

--- test/Coupon.test.ts
+++ test/Coupon.test.ts
@@ -1,3 +1,5 @@
+import Coupon from "../src/Coupon";
+
 test("Should create a valid discount coupon", function () {
   const input = new Coupon("DISCOUNT20", 20, new Date("2023-10-01T10:00:00"));
   expect(input.isExpired(new Date("2023-10-01T10:00:00"))).toBeFalsy();
