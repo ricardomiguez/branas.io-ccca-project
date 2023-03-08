207cd9fe0d045655107459ca381063f22881105c
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-08 23:08:37 +0000

    test: update shipping calculator resource class tests

--- test/ShippingCalculator.test.ts
+++ test/ShippingCalculator.test.ts
@@ -1,16 +1,8 @@
+import Product from "../src/Product";
 import ShippingCalculator from "../src/ShippingCalculator";
 
 test("Should calculate product shipping", async function () {
-  const input = {
-    idProduct: 6,
-    description: "A",
-    price: 1000,
-    width: 100,
-    height: 30,
-    length: 10,
-    weight: 3,
-    currency: "USD",
-  };
+  const input = new Product(6, "A", 1000, 100, 30, 10, 3, "USD");
   const output = ShippingCalculator.calculate(input);
   expect(output).toBe(30);
 });
