506fcc261903d2c26dc83e74f07c0bdc33964f73
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-08 23:10:16 +0000

    test: update product resource class tests

--- test/Product.test.ts
+++ test/Product.test.ts
@@ -1,3 +1,5 @@
+import Product from "../src/Product";
+
 test("Should not create a product with invalid dimensions", function () {
   expect(() => new Product(1, "A", 1000, -10, -10, -10, -10, "BRL")).toThrow(
     new Error("Invalid dimension")
