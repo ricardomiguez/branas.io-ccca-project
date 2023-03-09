8babc7b6d6ad38789bb11aa4f26f796a7dd1a8ce
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-09 22:32:29 +0000

    feat: update order repository interface

--- src/OrderRepository.ts
+++ src/OrderRepository.ts
@@ -1,5 +1,7 @@
+import Order from "./Order";
+
 export default interface OrderRepository {
-  save(order: any): Promise<void>;
-  getById(id: string): Promise<any>;
+  save(order: Order): Promise<void>;
+  getById(id: string): Promise<Order>;
   count(): Promise<number>;
 }
