1f99c915bad336d01f57383c785237575aeb0ac4
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-09 22:36:07 +0000

    feat: update order repository database class

--- src/OrderRepositoryDatabase.ts
+++ src/OrderRepositoryDatabase.ts
@@ -1,25 +1,33 @@
 import pgp from "pg-promise";
+import Item from "./Item";
+import Order from "./Order";
 import OrderRepository from "./OrderRepository";
 
 export default class orderRepositoryDatabase implements OrderRepository {
-  async save(order: any): Promise<void> {
+  async save(order: Order): Promise<void> {
     const connection = pgp()(
       "postgres://postgres:123456@localhost:5432/ricardomiguez"
     );
     await connection.query(
       "insert into ccca.order (id_order, tax_number, code, total, shipping) values ($1, $2, $3, $4, $5)",
-      [order.idOrder, order.taxNumber, order.code, order.total, order.shipping]
+      [
+        order.idOrder,
+        order.taxNumber,
+        order.code,
+        order.getTotal(),
+        order.shipping,
+      ]
     );
     for (const item of order.items) {
       await connection.query(
         "insert into ccca.item (id_order, id_product, price, quantity) values ($1, $2, $3, $4)",
-        [item.idOrder, item.idProduct, item.price, item.quantity]
+        [order.idOrder, item.idProduct, item.price, item.quantity]
       );
     }
     await connection.$pool.end();
   }
 
-  async getById(id: string): Promise<any> {
+  async getById(id: string): Promise<Order> {
     const connection = pgp()(
       "postgres://postgres:123456@localhost:5432/ricardomiguez"
     );
@@ -27,8 +35,29 @@ export default class orderRepositoryDatabase implements OrderRepository {
       "select * from ccca.order where id_order = $1",
       [id]
     );
+    const order = new Order(
+      orderData.id_order,
+      orderData.tax_number,
+      undefined,
+      1,
+      new Date()
+    );
+    const itemsData = await connection.query(
+      "select * from ccca.item where id_order = $1",
+      [id]
+    );
+    for (const itemData of itemsData) {
+      order.items.push(
+        new Item(
+          itemData.id_product,
+          parseFloat(itemData.price),
+          itemData.quantity,
+          "BRL"
+        )
+      );
+    }
     await connection.$pool.end();
-    return orderData;
+    return order;
   }
 
   async count(): Promise<number> {
