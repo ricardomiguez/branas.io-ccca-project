81a0fb6e006f7914b649942fb7a58a4ca7068006
ricardomiguez <ricardo.miguez@icloud.com>
2023-03-09 22:41:13 +0000

    feat: update simulate shipping resource class

--- src/SimulateShipping.ts
+++ src/SimulateShipping.ts
@@ -1,5 +1,6 @@
 import ProductRepository from "./ProductRepository";
 import ProductRepositoryDatabase from "./ProductRepositoryDatabase";
+import ShippingCalculator from "./ShippingCalculator";
 
 export default class SimulateShipping {
   constructor(
@@ -10,15 +11,8 @@ export default class SimulateShipping {
     const output: Output = { shipping: 0 };
     if (input.items) {
       for (const item of input.items) {
-        const productData = await this.productRepository.getProduct(
-          item.idProduct
-        );
-        const volume =
-          ((((productData.width / 100) * productData.height) / 100) *
-            productData.length) /
-          100;
-        const density = parseFloat(productData.weight) / volume;
-        const itemShipping = 1000 * volume * (density / 100);
+        const product = await this.productRepository.getProduct(item.idProduct);
+        const itemShipping = ShippingCalculator.calculate(product);
         output.shipping += Math.max(itemShipping, 10) * item.quantity;
       }
     }
